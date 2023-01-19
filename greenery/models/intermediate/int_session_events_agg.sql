d5a49eb702f55dff32f22ac45f2b65ab38452f44
Janio Martinez Bachmann <janiobachmann@gmail.com>
2023-01-19 16:21:35 +0000

    Adding models

--- greenery/models/intermediate/int_session_events_agg.sql
+++ greenery/models/intermediate/int_session_events_agg.sql
@@ -11,15 +11,15 @@ with events as (
 ),
 final as (
     select 
-        event_guid
+          user_guid
         , session_guid
         , sum(case when event_type = 'add_to_cars' then 1 else 0 end) as add_to_carts
         , sum(case when event_type = 'checkout' then 1 else 0 end) as checkout
         , sum(case when event_type = 'package_shipped' then 1 else 0 end) as package_shipped
         , sum(case when event_type = 'page_view' then 1 else 0 end) as page_view
-        , min(created_at_utc)
-        , max(created_at_utc)
-    from {{ ref('stg_postgres__events')}}
+        , min(created_at_utc) AS first_session_event_at_utc
+        , max(created_at_utc) AS last_session_event_at_utc
+    from events
     group by 1,2 
 )
 select * from final
---
+++ greenery/models/marts/product/fact/fct_user_session.sql
@@ -0,0 +1,31 @@
+{{
+  config(
+    materialized='table'
+  )
+}}
+
+
+with session_events_agg as (
+    select 
+        *
+    from {{ref('int_session_events_agg')}}
+)
+, users as (
+    select * from {{ref('stg_postgres__users')}}
+)
+select
+    session_events_agg.session_guid
+    , session_events_agg.user_guid
+    , users.first_name
+    , users.last_name
+    , users.email
+    , session_events_agg.page_view
+    , session_events_agg.checkout
+    , session_events_agg.add_to_carts
+    , session_events_agg.package_shipped
+    , session_events_agg.first_session_event_at_utc
+    , session_events_agg.last_session_event_at_utc
+    , datediff('minute', first_session_event_at_utc, last_session_event_at_utc) as session_length_minutes
+from session_events_agg
+left join users
+on session_events_agg.user_guid = users.user_guid
