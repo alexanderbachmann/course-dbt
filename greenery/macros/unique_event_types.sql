ba75464c055a4c4288c05a5d8812a2513e8a9f5a
Janio Martinez Bachmann <janiobachmann@gmail.com>
2023-01-22 19:17:07 +0000

    Adding customize macro

---
+++ greenery/macros/unique_event_types.sql
@@ -0,0 +1,21 @@
+{% macro unique_event_types() %} -- create macro
+
+{% set event_query %}  -- set distinct query
+select distinct
+event_type
+from stg_postgres__events
+{% endset %}
+
+{% set results = run_query(event_query) %} -- convenient way to run query and fetch the query above results
+
+-- assign results to result list if query executes
+-- and returns first column event_type in event_query
+{% if execute %}
+{% set results_list = results.columns[0].values() %}
+{% else %}
+{% set results_list = [] %}
+{% endif %}
+
+{{ return(results_list) }} -- return first column
+
+{% endmacro %}
--- greenery/models/intermediate/int_session_events_agg.sql
+++ greenery/models/intermediate/int_session_events_agg.sql
@@ -4,6 +4,9 @@
   )
 }}
 
+-- Calling our macro with unique event types
+{%- set unique_types = unique_event_types() -%}
+
 with events as (
     select 
         *
@@ -19,6 +22,9 @@ final as (
         , sum(case when event_type = 'page_view' then 1 else 0 end) as page_view
         , min(created_at_utc) AS first_session_event_at_utc
         , max(created_at_utc) AS last_session_event_at_utc
+        ,  {%- for unique_type in unique_types %}
+    , sum(case when event_type = '{{unique_type}}' then 1 else 0 end) as {{unique_type}}
+    {%- endfor %}
     from events
     {{ dbt_utils.group_by(2) }}
 )
