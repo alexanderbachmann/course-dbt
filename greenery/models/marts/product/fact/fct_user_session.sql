93188f69b451a28ce00a6c0ec2b0fcde6f6a6c9f
Janio Martinez Bachmann <janiobachmann@gmail.com>
2023-01-25 07:25:29 +0000

    correcting mispelling

--- greenery/models/marts/product/fact/fct_user_session.sql
+++ greenery/models/marts/product/fact/fct_user_session.sql
@@ -21,7 +21,7 @@ select
     , users.email
     , session_events_agg.page_view
     , session_events_agg.checkout
-    , session_events_agg.add_to_carts
+    , session_events_agg.add_to_cart
     , session_events_agg.package_shipped
     , session_events_agg.first_session_event_at_utc
     , session_events_agg.last_session_event_at_utc
